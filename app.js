8c0f220cc91dab9b87dd4b7ac81f7fb256b8f7c2
Conor O'Flanagan <oflanac52@gmail.com>
2018-06-03 00:22:07 +0100

    basic login with passport and jwt's

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import cors from 'cors';
+import passport from 'passport';
+import jwt from 'jsonwebtoken';
 import graphqlHTTP from 'express-graphql';
 
 import ArticleSchema from './graphql/index';
@@ -12,6 +14,7 @@ import SiteEntry from './models/SiteEntry';
 import Article from './models/Article';
 import scrapeArticles from './scrapeArticles';
 
+require('./auth/auth');
 
 const app = express();
 
@@ -63,6 +66,35 @@ app.get('/getarticles', (req, res) => {
 app.get('/deleteOldest', (req, res) => {
   Article.deleteOldest(parseInt(req.query.size, 10) || 1);
   res.json(`Deleted ${parseInt(req.query.size, 10) || 1} articles`);
+});
+
+app.post('/register', passport.authenticate('register', { session: false }), async (req, res) => {
+  res.json({
+    message: 'Signup successful'
+  });
+});
+
+app.post('/login', async (req, res) => {
+  passport.authenticate('login', async (err, user, message) => {
+    console.log(message);
+    try {
+      if (err || !user) {
+        return res.json(401, message);
+      }
+      req.login(user, { session: false }, async (error) => {
+        if (error) return res.json({ error });
+        // We don't want to store the sensitive information such as the
+        // user password in the token so we pick only the email and id
+        const body = { _id: user._id, email: user.email };
+        // Sign the JWT token and populate the payload with the user email and id
+        const token = jwt.sign({ user: body }, 'top_secret');
+        // Send back the token to the user
+        return res.json({ token });
+      });
+    } catch (error) {
+      return res.json(error);
+    }
+  })(req, res);
 });
 
 export default app;
